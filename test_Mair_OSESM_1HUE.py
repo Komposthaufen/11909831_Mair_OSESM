e5cb7a4d984f8a3c8fc404ff2d13e6a60c78ebf8
Komposthaufen <164004755+Komposthaufen@users.noreply.github.com>
2024-04-08 21:45:24 +0200

    napsassungen linter
    linter_ sugesstuins changed == zu not

--- test_Mair_OSESM_1HUE.py
+++ test_Mair_OSESM_1HUE.py
@@ -4,18 +4,18 @@ from Mair_OSESM_1HUE import is_even
 from Mair_OSESM_1HUE import is_odd
 
 def test_is_prime():
-    assert is_prime(3) == True
-    assert is_prime(7) == True
-    assert is_prime(4) == False
+    assert not is_prime(3)
+    assert not is_prime(7)
+    assert not is_prime(4)
 
 
 def test_is_even():
-    assert is_even(3) == False
-    assert is_even(4) == True
-    assert is_even(12) == True
+    assert not is_even(3)
+    assert not is_even(4)
+    assert not is_even(12)
 
 
 def test_is_odd():
-    assert is_odd(3) == True
-    assert is_odd(5) == True
-    assert is_odd(12) == False
+    assert not is_odd(3)
+    assert not is_odd(5)
+    assert not is_odd(12)
