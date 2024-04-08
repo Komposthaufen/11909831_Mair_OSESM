41a945673385abeff9412b04a3cc4b528c87a1cc
Komposthaufen <164004755+Komposthaufen@users.noreply.github.com>
2024-04-08 22:12:27 +0200

    Linting
    Linting 1 more line space

--- Mair_OSESM_1HUE.py
+++ Mair_OSESM_1HUE.py
@@ -26,7 +26,8 @@ print("Pot:", Pot)
 #3 Funktionen
 def is_prime(n):
     #Check if a number is prime.
-    if n <= 1: return False
+    if n <= 1: 
+        return False
     return all(n % i != 0 for i in range(2, int(n**0.5) + 1))
 
 
