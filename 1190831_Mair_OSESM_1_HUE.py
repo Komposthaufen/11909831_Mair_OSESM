89b53b75308987561bc468f3b904687e8fff415a
Komposthaufen <164004755+Komposthaufen@users.noreply.github.com>
2024-04-08 20:01:22 +0200

    Update 1190831_Mair_OSESM_1_HUE.py
    Linting_2

--- 1190831_Mair_OSESM_1_HUE.py
+++ 1190831_Mair_OSESM_1_HUE.py
@@ -24,6 +24,7 @@ vec2 = np.array(y)
 print(vec2)
 print("Dimensionen:", np.shape(y))
 
+# Rechnungen
 Summe = v_1 + v_2
 Produkt = v_1 * v_1
 Sub = v_1 - v_1
@@ -36,22 +37,28 @@ print("Sub:", Sub)
 print("Div:", Div)
 print("Pot:", Pot)
 
+
+#4 Funktionen
 def factorial(n):
     #Calculate the factorial of a non-negative integer.
     return 1 if n == 0 else n * factorial(n-1)
+
 
 def is_prime(n):
     #Check if a number is prime.
     if n <= 1: return False
     return all(n % i != 0 for i in range(2, int(n**0.5) + 1))
 
+
 def is_even(n):
     #Check if a number is even.
     return n % 2 == 0
 
+
 def is_odd(n):
     #Check if a number is odd.
     return n % 2 != 0
+
 
 # Calling the  functions with v_1
 v_1_factorial = factorial(v_1)
